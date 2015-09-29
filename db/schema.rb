1262b0850a538ae456ea1081fce686d0c6919cd0
koudaiii <cs006061@gmail.com>
2015-09-29 09:18:39 +0900

    Add modle Book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927155031) do
+ActiveRecord::Schema.define(version: 20150929001615) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.date     "published_on"
+    t.integer  "price"
+    t.integer  "number_of_page"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
---
+++ spec/factories/books.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :book do
+    name "MyString"
+published_on "2015-09-29"
+price 1
+number_of_page 1
+  end
+
+end
