b20787f59621b3a41a23209abeac2d7c1a32c2dd
koudaiii <cs006061@gmail.com>
2015-10-01 09:10:45 +0900

    be rails g migration AddPublisherIdToBooks publisher:references

---
+++ db/migrate/20151001000953_add_publisher_id_to_books.rb
@@ -0,0 +1,5 @@
+class AddPublisherIdToBooks < ActiveRecord::Migration
+  def change
+    add_reference :books, :publisher, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001000832) do
+ActiveRecord::Schema.define(version: 20151001000953) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20151001000832) do
     t.integer  "number_of_page"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "publisher_id"
   end
+
+  add_index "books", ["publisher_id"], name: "index_books_on_publisher_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
