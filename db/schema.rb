b7c22a9612a7d70b7758312282d8c5fd651c8092
koudaiii <cs006061@gmail.com>
2015-10-01 09:18:02 +0900

    be rails g model BookAuthor book:references author:references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001000953) do
+ActiveRecord::Schema.define(version: 20151001001536) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20151001000953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id"
+  add_index "book_authors", ["book_id"], name: "index_book_authors_on_book_id"
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/book_authors.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :book_author do
+    book nil
+author nil
+  end
+
+end
