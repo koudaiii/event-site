1af4ed96bbd60b3572d94c6f81b82796c58278cc
koudaiii <cs006061@gmail.com>
2015-10-05 09:20:45 +0900

    Add before_aciton in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def show
-    @book = Book.find(paramas[:id])
     respond_to do |format|
       format.html
       format.csv
       format.json
     end
   end
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
 end
