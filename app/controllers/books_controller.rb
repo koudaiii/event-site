60e42e8c22297d1781fa7e42a358d58800d0fa85
koudaiii <cs006061@gmail.com>
2015-10-05 09:16:13 +0900

    Add method show in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,10 @@
 class BooksController < ApplicationController
+  def show
+    @book = Book.find(paramas[:id])
+    respond_to do |format|
+      format.html
+      format.csv
+      format.json
+    end
+  end
 end
