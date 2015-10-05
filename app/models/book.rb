4ced5422be1b4688ba6fb6f2dbedc4a1a61e7418
koudaiii <cs006061@gmail.com>
2015-10-05 09:11:55 +0900

    fix beautiful code

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
-  enum status: %w(reservation now_on_sale end_of_print)
-  scope :costly, -> { where("price > ?", 3000)}
-  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+  enum status: { reservation: 0, now_on_sale: 1, end_of_print: 2 }
+  scope :costly, -> { where('price > ?', 3000) }
+  scope :written_about, ->(theme) { where('name like ?', "%#{theme}%") }
 
   belongs_to :publisher
 
@@ -12,18 +12,18 @@ class Book < ActiveRecord::Base
   validates :name, length: { maximum: 15 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validate do |book|
-    if book.name.include?("exercise")
+    if book.name.include?('exercise')
       book.errors[:name] << "I don't like exercise."
     end
   end
   before_validation :add_lovely_to_cat
-  after_destroy :if => :high_price? do |book|
+  after_destroy if: :high_price? do |book|
     Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
-    Rails.logger.warn "Please Check!!"
+    Rails.logger.warn 'Please Check!!'
   end
 
   def add_lovely_to_cat
-    self.name = self.name.gsub(/Cat/) do |matched|
+    self.name = name.gsub(/Cat/) do |matched|
       "lovely #{matched}"
     end
   end
