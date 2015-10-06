f4b125308c32acf20274c912fdb662d61397f785
koudaiii <cs006061@gmail.com>
2015-10-06 09:14:51 +0900

    Add resource in Profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005000534) do
+ActiveRecord::Schema.define(version: 20151006001306) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 20151005000534) do
   create_table "publishers", force: :cascade do |t|
     t.string   "name"
     t.text     "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.string   "email"
+    t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+password "MyString"
+email "MyString"
+admin false
+  end
+
+end
