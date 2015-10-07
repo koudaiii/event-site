afb37e2934a591b4b96194f03a30be031405bf89
koudaiii <cs006061@gmail.com>
2015-10-07 09:29:52 +0900

    Add show in Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   def show
-    respond_to do |format|
-      format.html
-      format.csv
-      format.json
-    end
+    render :show
   end
 
   private
