07a7467fa7ced742ed28fa3af63f08cafc72be5b
koudaiii <cs006061@gmail.com>
2015-10-07 09:23:02 +0900

    Profile update

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,13 @@ class ProfileController < ApplicationController
   end
 
   def update
+    user = current_user
+    user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
