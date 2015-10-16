4a2a185771ffe9c1e5b9bc67bb3e6c9492f015a1
koudaiii <cs006061@gmail.com>
2015-10-16 09:25:56 +0900

    Add decorator

--- app/decorators/user_decorator_decorator.rb
+++ app/decorators/user_decorator_decorator.rb
@@ -1,3 +1,10 @@
 # coding: utf-8
 module UserDecoratorDecorator
+  def email_link(body = nil, &block)
+    if block_given?
+      link_to("nailto:#{email}", &block)
+    else
+      link_to(body, "mailto:#{email}")
+    end
+  end
 end
