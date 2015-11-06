76c012c63580d64da65b3fb789671f3f12efad4b
koudaiii <cs006061@gmail.com>
2015-11-06 09:23:43 +0900

    Add create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,13 @@ class EventsController < ApplicationController
   def new
     @event = current_user.created_events.build
   end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: '作成しました'
+    else
+      render :new
+    end
+  end
 end
