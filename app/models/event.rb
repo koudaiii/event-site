19ef961806843312a38fb38c332fa0c0d7d6f3c1
koudaiii <cs006061@gmail.com>
2015-11-20 09:27:21 +0900

    Add relation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  has_many :tickets
   belongs_to :owner, class_name: 'User'
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
