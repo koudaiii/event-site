7d349fe2c94efef5cb71a7c177a3e0b32e9b66a7
koudaiii <cs006061@gmail.com>
2015-11-23 14:56:38 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :tickets
+  end
+
   root to: 'welcom#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
