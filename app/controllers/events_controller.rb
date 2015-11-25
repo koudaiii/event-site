910705b747700cefb0df816bbf2c6cdd251ddee2
koudaiii <cs006061@gmail.com>
2015-11-25 09:24:51 +0900

    get tickets

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,6 +39,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
   private
