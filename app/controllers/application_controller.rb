8be0c25da4f345075b9a793ec4150f45c1a5cf36
koudaiii <cs006061@gmail.com>
2015-11-30 09:19:10 +0900

    Add rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,19 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
   helper_method :current_user
 
+  rescue_from Exception, with: :rescue_from
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  def error404(e)
+    logger.error [e, *e.backtrace].join("¥n")
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join("¥n")
+    render 'error500', status: 500, formats: [:html]
+  end
+
   private
 
   def current_user
