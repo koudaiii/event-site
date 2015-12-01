2b3696ad93801da806acc74813843d34bc2741f5
koudaiii <cs006061@gmail.com>
2015-12-01 09:25:58 +0900

    Add pagename

--- app/controllers/welcom_controller.rb
+++ app/controllers/welcom_controller.rb
@@ -1,5 +1,8 @@
 class WelcomController < ApplicationController
+  PER = 10
+
   def index
-    @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
+    @events = Event.page(params[:page]).per(PER).
+      where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
