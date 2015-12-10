3f850128bd74f0911fbdb3d6a0b071878f549ad8
koudaiii <cs006061@gmail.com>
2015-12-10 09:45:26 +0900

    TODO: fix

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  describe '#name' do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(50) }
+  end
 end
