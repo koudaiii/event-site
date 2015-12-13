a57f4c1cadc2c44e7936b9671e25bbe971c22e89
koudaiii <cs006061@gmail.com>
2015-12-13 23:26:51 +0900

    Add controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  describe 'GET #new' do
+    context '未ログインユーザがアクセスしたとき' do
+      before { get :new }
 
+      it 'トップページにリダイレクトすること' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'ログインユーザがアクセスしたとき' do
+      before do
+        user = create :user
+        session[:user_id] = user.id
+        get :new
+      end
+
+      it 'ステータスコードとして200が返ること' do
+        expect(response.status).to eq(200)
+      end
+
+      it '@eventに、新規Eventオブジェクトが格納されていること' do
+        expect(assigns(:event)).to be_a_new(Event)
+      end
+    end
+  end
 end
