5328ed193409710cdf5bb2393afb77abe5b43d1c
koudaiii <cs006061@gmail.com>
2015-12-14 09:25:19 +0900

    add spec

--- spec/views/events/show.html.haml_spec.rb
+++ spec/views/events/show.html.haml_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "events/show", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '未ログインユーザーがアクセスしたとき' do
+    before do
+      allow(view).to receive(:logged_in?) { false }
+      allow(view).to receive(:current_user) { nil }
+    end
+
+    context 'かつ @event.owner が nil のとき' do
+      before do
+        assign(:event, create(:event, owner: nil))
+        assign(:tickets, [])
+      end
+
+      it '"退会したユーザーです"と表示されていること' do
+        render
+        expect(rendered).to match /退会したユーザーです/
+      end
+    end
+  end
 end
