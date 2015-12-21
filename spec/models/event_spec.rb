f71009e8df3e9ad503ed6051e8a345d7d937103e
koudaiii <cs006061@gmail.com>
2015-12-21 09:51:24 +0900

    add spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -17,6 +17,24 @@
 require 'rails_helper'
 
 describe Event do
+
+  describe '#rails?' do
+    context '#name が "Rails勉強会" のとき' do
+      it 'true を返すこと' do
+        event = create(:event, name: 'Rails勉強会')
+        expect(event.rails?).to eq true
+      end
+    end
+
+    context '#name が "Ruby勉強会" のとき' do
+      it 'true を返すこと' do
+        event = create(:event, name: 'Ruby勉強会')
+        expect(event.rails?).to eq false
+      end
+    end
+  end
+
+
   describe '#name' do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name) }
